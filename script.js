8c65741a42ff5bef3a877cd226b1dcec50a0d5dc
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-01-23 22:47:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
 // your code here
+	let x=0;
+	let y=1;
+	let z=0;
+	if(num<=1)return num;
+	for(let n=0;n<num;n++){
+		z=x+y;
+		x=y;
+		y=z;
+	}
+	return z;
 }
 
 module.exports = fibonacci;
